c19e6a9b8d7d272cecd99171e2aa3d6664b1433d
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-03-11 19:34:29 +0100

    First attempt at track search cypress test and modifies App.jsx for button and input field

--- cypress/integration/userCanFindSong.feature.js
+++ cypress/integration/userCanFindSong.feature.js
@@ -1,12 +1,15 @@
 describe("user can search for song", () => {
-  it ("retrieves song from API", () => {
-      cy.visit("/");
-      //cy.get("click#searchsong").type("Vertigo");
-      //cy.get("select#song").select("song");
-      cy.get("input#song").type("Vertigo");
-      cy.get("p#song-message").should(
-          "contain",
-          "U2"
-          );
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("retrieves song from API", () => {
+    // cy.get("select#selectmethod").select("track");
+    cy.get("input#tracks").type('Vertigo');
+    cy.get("button#search").click();
+    cy.get("#tracks").should(
+      "contain",
+      "Vertigo"
+    );
   });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
-
+import React, { Component } from "react";
 
 class App extends Component {
+
+  state={
+    query: ''
+  }
+
+  onChangeHandler = e => {
+    const input = e.target.value
+    this.setState({ [e.target.name]: input });
+  }
+
+  onSubmitHandler = e => {
+    e.preventDefault();
+    //this.props.fetchSearchedTracks(this.state.query)
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <form
+        onSubmitHandler={this.onSubmitHandler}
+      >
+        <input
+        id="tracks"
+        type="text"
+        name="query"
+        value={this.state.query}
+        onChangeHandler={this.onChangeHandler}/>
+        <button id="search"> Search</button>
+      </form>
+    );
   }
 }
 
