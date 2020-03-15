d55b01a98a9a9e437d1c94590b2bd5af1cff976e
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-15 16:49:01 +0100

    Adding user has to log in to render app

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,34 @@ import React, { Component } from "react";
 import UserCanSearchSong from "./components/UserCanSearchSong";
 import UserCanSearchArtist from "./components/UserCanSearchArtist";
 import FacebookLogin from "./components/FacebookLogin";
+import LoginForm from "./Components/LoginForm";
 
 class App extends Component {
   state = {
-    tracks: "",
-    artists: ""
+    renderLoginForm: false
   }
 
   onChangeHandler = e => {
-    this.setState({ [e.track.name]: e.track.name });
+    this.setState({ [e.target.name]: e.target.value });
   };
 
-
-
   render() {
+    const renderLogin = this.state.renderLoginForm ? (
+      <LoginForm />
+    ) : (
+      <button
+        id="login"
+        onClick={() => this.setState({ renderLoginForm: true })}
+      >
+        Login
+      </button>
     return (
       <>
+        <div>
+          <InputFields onChangeHandler={this.onChangeHandler} />
+          {renderLogin}
+        </div>
+
         <div>
           <FacebookLogin />
         </div>
