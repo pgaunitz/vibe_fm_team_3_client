346a1d39f1ffdbdb2ffc2bfeeb0dfa305bdb219c
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-15 16:52:20 +0100

    adding submition form handler

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,40 @@ import LoginForm from "./Components/LoginForm";
 
 class App extends Component {
   state = {
-    renderLoginForm: false
+    renderLoginForm: false,
+    authenticated: false,
+    message: ""
   }
 
   onChangeHandler = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  onLogin = async e => {
+    e.preventDefault();
+    const response = await authenticate(
+      e.target.email.value,
+      e.target.password.value
+    );
+    if (response.authenticated) {
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ message: response.message, renderLoginForm: false });
+    }
+  };
+
   render() {
     const renderLogin = this.state.renderLoginForm ? (
-      <LoginForm />
+      <LoginForm submitFormHandler={this.onLogin} />
     ) : (
-      <button
-        id="login"
-        onClick={() => this.setState({ renderLoginForm: true })}
-      >
-        Login
-      </button>
+        <button
+          id="login"
+          onClick={() => this.setState({ renderLoginForm: true })}
+        >
+          Login
+        </button>
+      );
+      
     return (
       <>
         <div>
