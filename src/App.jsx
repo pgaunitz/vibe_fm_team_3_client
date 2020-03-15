2f65c07b57702dc999d1ead82a2305df30ba4388
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-15 11:21:50 +0100

    added facebook button, pop up working

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import React, { Component } from "react";
 import UserCanSearchSong from "./components/UserCanSearchSong";
 import UserCanSearchArtist from "./components/UserCanSearchArtist";
+import { FacebookProvider, LoginButton } from 'react-facebook';
 
 class App extends Component {
   state = {
     tracks: "",
     artists: ""
   }
+
+
+  handleResponse = data => {
+    debugger;
+  } 
+
   onChangeHandler = e => {
     this.setState( { [e.track.name]: e.track.name });
   };
 
+
+
   render() {
     return (
       <>
+      <div>
+      <FacebookProvider appId="869256990162492">
+        <LoginButton
+          scope="email"
+          onCompleted={this.handleResponse}
+        >
+          <span>Login via Facebook</span>
+        </LoginButton>
+      </FacebookProvider>
+      </div>
+
         <label> Search by track</label>
        {<UserCanSearchSong />}
 
